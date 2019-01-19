59d68d3952a9413d5947a6fbc4f6a0b5d0202a88
landalfYao <37137415+landalfYao@users.noreply.github.com>
2019-01-19 12:41:13 +0800

    111

--- src/pages/wallet/wallet.js
+++ src/pages/wallet/wallet.js
@@ -2,14 +2,48 @@ let that;
 let list = {
   data() {
     return {
-
+      msg:'',
+      tableData:[],
+      total:0,
+      query:{
+        tables:'capital_trend,helplist',
+        fields:'capital_trend.*,helplist.order_num,helplist.total_fee',
+        sorts:'capital_trend.create_time desc',
+        wheres:'capital_trend.h_id = helplist.id  ',
+        pageIndex:1,
+        pageSize:10,
+      }
     }
   },
   mounted() {
     that = this;
+    this.getMsg()
+    this.getList()
   },
   methods: {
-
+    getMsg(){
+      this.yzy.post('ct/get',{
+        wheres:'type=2 and uid='+sessionStorage.getItem("uid"),
+        tables:'wallets',
+        sorts:'id asc'
+      },function(res){
+        if(res.code == 1){
+          that.msg = res.data.list[0]
+        }
+      })
+    },
+    getList(){
+      if(sessionStorage.getItem("a_id")){
+        this.query.wheres += ' and capital_trend.a_id='+sessionStorage.getItem("a_id")
+      }
+      
+      this.yzy.post('ct/get',this.query,function(res){
+        if(res.code == 1){
+          that.tableData = res.data.list
+          that.total = res.data.total
+        }
+      })
+    }
   }
 }
 module.exports = list
--- src/pages/wallet/wallet.vue
+++ src/pages/wallet/wallet.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>121</div>
+  <div>
+    <el-card shadow="always" style="width:33.33%;display:inline-block">
+      <div>我的总收入</div>
+      <div class="ma-t20" style="font-size:30px"> <span class="fo-32">￥</span>{{msg.income_total || 0}}</div>
+    </el-card>
+    <el-card shadow="always" style="width:33%;display:inline-block">
+      <div>账户余额</div>
+      <div class="ma-t20" style="font-size:30px"> <span class="fo-32">￥</span>{{(msg.income_total-msg.cash) || 0}}</div>
+    </el-card>
+    <el-card shadow="always" style="width:33%;display:inline-block">
+      <div>已提现</div>
+      <div class="ma-t20" style="font-size:30px"> <span class="fo-32">￥</span>{{msg.cash || 0}}</div>
+    </el-card>
+    <el-table
+      :data="tableData"
+      ref="multipleTable"
+      tooltip-effect="dark"
+      border
+      size="small"
+      style="width: 100%;margin-top:15px"
+    >
+      <el-table-column prop="order_num" label="单号"></el-table-column>
+      <el-table-column prop="p_get" label="平台收益"></el-table-column>
+      <el-table-column prop="a_get" label="代理收益"></el-table-column>
+      <el-table-column prop="u_get" label="用户收益"></el-table-column>
+      <el-table-column prop="rate" label="收益率"></el-table-column>
+      <el-table-column prop="total_fee" label="单笔金额"></el-table-column>
+    </el-table>
+  </div>
 </template>
 <script>
 export default require("./wallet.js");
