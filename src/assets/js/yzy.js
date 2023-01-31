88f2c3067e34c804d6635739447aa4eaf1cd955c
admin <1062845500@qq.com>
2023-01-31 14:41:22 +0800

    新版本修复

--- src/assets/js/yzy.js
+++ src/assets/js/yzy.js
@@ -10,7 +10,7 @@ const request = require("request");
 const yzy = {
   systemName: "校园综合服务平台",
 
-  NODE_API: "http://paotui.dianpao.com.cn/api/",
+  NODE_API: "http://localhost:3337/api/",
   pageSize: [{
       label: "每页10条",
       value: 10
